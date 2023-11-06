096b04ccfca96d6094b719e0678c2a58d93b010f
AAlchi <alialchi07@gmail.com>
2023-11-06 10:33:36 -0600

    Initial Commit

---
+++ CPB Firebase/CPB_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPB_FirebaseApp.swift
+//  CPB Firebase
+//
+//  Created by Ali Alchikh Ibrahim on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CPB_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
